cee6224dee5c4fc3c1299765561016883b1eb4c0
Sachin Sharma <udgamsachin@gmail.com>
2021-08-24 08:07:58 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
 
+
 makeCacheMatrix <- function(x = matrix()) {
+  ## @x: a square invertible matrix
+  ## return: a list containing functions to
+  ##              1. set the matrix
+  ##              2. get the matrix
+  ##              3. set the inverse
+  ##              4. get the inverse
+  ##         this list is used as the input to cacheSolve()
   z <- NULL
   set <- function(y) {
+    # use `<<-` to assign a value to an object in an environment 
+    # different from the current environment. 
     x <<- y
     z <<- NULL
   }
@@ -15,13 +25,19 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 cacheSolve <- function(x, ...) {
+  ## @x: output of makeCacheMatrix()
+  ## return: inverse of the original matrix input to makeCacheMatrix()
   z <- x$getinverse()
+  # if the inverse has already been calculated
   if (!is.null(z)) {
+    # get it from the cache and skips the computation.
     message("getting cached data")
     return(z)
   }
+  # otherwise, calculates the inverse 
   data <- x$get()
   z <- solve(data, ...)
+  # sets the value of the inverse in the cache via the setinv function.
   x$setinverse(z)
   z
 }
